d3e6d3250301abc67ee111d81a2c1c626e8fe5b3
Arm <149869823+Dravyn01@users.noreply.github.com>
2025-11-14 12:11:27 +0700

    add message for dto

--- src/modules/stock/dto/create-movement.dto.ts
+++ src/modules/stock/dto/create-movement.dto.ts
@@ -9,30 +9,30 @@ import {
 } from 'class-validator';
 import { StockChangeType } from '../enums/stock-change.enum';
 
-// TODO: add message
-
 export class CreateMovementDTO {
-  @IsNotEmpty({ message: '' })
-  @Min(1, { message: '' })
-  @IsInt({ message: '' })
+  @IsNotEmpty({ message: 'ต้องระบุ variant_id' })
+  @Min(1, { message: 'variant_id ต้องมีค่ามากกว่า 0' })
+  @IsInt({ message: 'variant_id ต้องเป็นตัวเลขจำนวนเต็ม' })
   variant_id: number;
 
-  @IsNotEmpty({ message: '' })
-  @IsInt({ message: '' })
+  @IsNotEmpty({ message: 'ต้องระบุ quantity' })
+  @IsInt({ message: 'quantity ต้องเป็นตัวเลขจำนวนเต็ม' })
   quantity: number;
 
-  @IsNotEmpty({ message: '' })
-  @Min(1, { message: '' })
-  @IsIn([StockChangeType])
+  @IsNotEmpty({ message: 'ต้องระบุ change_type' })
+  @Min(1, { message: 'change_type ต้องเป็นค่าที่ถูกต้อง' })
+  @IsIn(Object.values(StockChangeType), {
+    message: `change_type ต้องเป็นหนึ่งในค่า: ${Object.values(StockChangeType).join(', ')}`,
+  })
   change_type: StockChangeType;
 
   @IsOptional()
-  @Length(1, 255, { message: '' })
-  @IsString({ message: '' })
+  @Length(1, 255, { message: 'note ต้องมีความยาว 1 ถึง 255 ตัวอักษร' })
+  @IsString({ message: 'note ต้องเป็น string' })
   note?: string;
 
   @IsOptional()
-  @IsInt({ message: '' })
-  @Min(1, { message: '' })
+  @IsInt({ message: 'order_id ต้องเป็นตัวเลขจำนวนเต็ม' })
+  @Min(1, { message: 'order_id ต้องมีค่ามากกว่า 0' })
   order_id?: number;
 }
