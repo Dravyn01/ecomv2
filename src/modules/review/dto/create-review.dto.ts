df70bb964e935520ebe8643aee3c3add4dd19565
Arm <149869823+Dravyn01@users.noreply.github.com>
2025-11-25 19:51:06 +0700

    Update create-review.dto.ts

--- src/modules/review/dto/create-review.dto.ts
+++ src/modules/review/dto/create-review.dto.ts
@@ -1,24 +1,21 @@
-import { IsInt, IsNotEmpty, IsString, Length, Max, Min } from 'class-validator';
-
-// TODO: add message
+import { IsPosative, IsInt IsNotEmpty, IsString, Length, Max, Min } from 'class-validator';
 
 export class CreateReviewDto {
   @IsNotEmpty({ message: '' })
   @IsInt({ message: '' })
   user_id: number;
 
-  @IsNotEmpty({ message: '' })
-  @IsInt({ message: '' })
+  @IsNotEmpty({ message: 'กรุณาเลือกสินค้าที่ต้องการรีวิว' })
+  @IsPosative({ message: 'รหัสสินค้าต้องเป็นตัวเลขที่มากกว่า 0' })
   variant_id: number;
 
-  @IsNotEmpty({ message: '' })
-  @IsInt({ message: '' })
-  @Min(1, { message: '' })
-  @Max(5, { message: '' })
+  @IsNotEmpty({ message: 'กรุณาเลือกจำนวนดาว' })
+  @IsInt({ message: 'จำนวนดาวต้องเป็นตัวเลข' })
+  @Min(1, { message: 'จำนวนดาวต้องไม่ต่ำกว่า 1 ดาว' })
+  @Max(5, { message: 'จำนวนดาวต้องไม่เกิน 5 ดาว' })
   rating: number;
 
-  @IsNotEmpty({ message: '' })
-  @Length(1, 255)
-  @IsString()
+  @IsNotEmpty({ message: 'กรุณากรอกความคิดเห็นต่อสินค้า' })
+  @Length(1, 100, { message: 'ความคิดเห็นต้องไม่สั้นเกินไป และ ยาวเกิน 100 ตัวอักษร' })
   comment: string;
 }
