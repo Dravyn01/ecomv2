1186e9a6ba343893857c181ab2436e07a06ef8ba
Arm <149869823+Dravyn01@users.noreply.github.com>
2025-11-25 20:33:25 +0700

    Update add-to-wishlist.dto.ts

--- src/modules/wishlist/dto/add-to-wishlist.dto.ts
+++ src/modules/wishlist/dto/add-to-wishlist.dto.ts
@@ -1,6 +1,7 @@
 import { IsInt } from 'class-validator';
 
 export class AddToWishlistDto {
-  @IsInt()
+  @IsInt({message:'กรุณาเลือกสินค้าที่ต้องการเพิ่มลงรายการโปรด'})
+@IsPosative({ message: 'รหัสสินค้าต้องเป็นจำนวนเต็มและมากกว่า 0' })
   product_id: number;
 }
