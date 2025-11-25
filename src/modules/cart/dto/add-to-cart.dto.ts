9f7fcb0b65d1e95077d4842447067358d96b7f3d
Arm <149869823+Dravyn01@users.noreply.github.com>
2025-11-25 19:32:33 +0700

    Update add-to-cart.dto.ts

--- src/modules/cart/dto/add-to-cart.dto.ts
+++ src/modules/cart/dto/add-to-cart.dto.ts
@@ -4,11 +4,11 @@ export class AddToCartDTO {
   @IsPositive()
   user_id: number; // can use jwtGuard and @Role()
 
-  @IsNotEmpty({ message: '' })
-  @IsPositive({ message: '' })
+  @IsNotEmpty({ message: 'กรุณาเลือกสินค้า' })
+  @IsPositive({ message: 'รหัสสินค้าต้องเป็นตัวเลขที่มากกว่า 0' })
   variant_id: number;
 
-  @IsNotEmpty({ message: '' })
-  @IsPositive({ message: '' })
+  @IsNotEmpty({ message: 'กรุณากรอกจำนวน' })
+  @IsPositive({ message: 'จำนวนต้องเป็นตัวเลขที่มากกว่า 0' })
   quantity: number;
 }
