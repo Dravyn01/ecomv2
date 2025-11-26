19a66468e1c446bd25bbd6feae91a0b810523f28
Arm <149869823+Dravyn01@users.noreply.github.com>
2025-11-26 12:49:34 +0700

    Update login.dto.ts

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -5,7 +5,7 @@ export class LoginRequest {
   @IsEmail({}, { message: 'รูปแบบอีเมลไม่ถูกต้อง' })
   email: string;
 
-  @IsString()
+ @IsNotEmpty({ message: 'รหัสผ่านต้องไม่เป็นค่าว่าง' })
   @MinLength(6, { message: 'รหัสผ่านต้องยาวกว่าหรือเท่ากับ 6 ตัวอักษร' })
   password: string;
 }
